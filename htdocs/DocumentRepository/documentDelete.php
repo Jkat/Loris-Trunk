78ad37e342700e707e5fc3661b94cfc8a1036900
Jkat <fission@live.com>
2014-07-04 16:34:34 -0400

    phpcs

--- htdocs/DocumentRepository/documentDelete.php
+++ htdocs/DocumentRepository/documentDelete.php
@@ -10,30 +10,33 @@ $config = NDB_Config::singleton();
 
 // create Database object
 $DB =& Database::singleton();
-if(PEAR::isError($DB)) { 
-    print "Could not connect to database: ".$DB->getMessage()."<br>\n"; die(); 
+if (PEAR::isError($DB)) {
+    print "Could not connect to database: ".$DB->getMessage()."<br>\n"; die();
 }
 
 $rid = $_POST['id'];
 
-$fileName = $DB->pselectOne("Select File_name from document_repository where record_id =:identifier", array(':identifier' => $rid)); 
-$userName = $DB->pselectOne("Select uploaded_by from document_repository where record_id =:identifier", array(':identifier'=> $rid)); 
+$fileName = $DB->pselectOne("Select File_name from document_repository where record_id =:identifier",
+                            array(':identifier' => $rid));
+$userName = $DB->pselectOne("Select uploaded_by from document_repository where record_id =:identifier",
+                            array(':identifier'=> $rid));
 
 $user =& User::singleton();
 if (Utility::isErrorX($user)) {
-        return PEAR::raiseError("User Error: ".$user->getMessage());
+    return PEAR::raiseError("User Error: ".$user->getMessage());
 }
 
-if ($user->hasPermission('file_upload')) { //if user has document repository permission
-	$DB->delete("document_repository", array("record_id" => $rid));
-        $www = $config->getSetting('www');
-        $msg_data['deleteDocument'] = $www['url'] . "/main.php?test_name=document_repository";
-        $msg_data['document'] = $fileName;
-        $query_Doc_Repo_Notification_Emails = "SELECT Email from users where Active='Y' and Doc_Repo_Notifications='Y'";
-        $Doc_Repo_Notification_Emails = $DB->pselect($query_Doc_Repo_Notification_Emails, array());        
-        foreach ($Doc_Repo_Notification_Emails as $email) {
-                Email::send($email['Email'], 'document_repository.tpl', $msg_data);
-        }
+//if user has document repository permission
+if ($user->hasPermission('file_upload')) {
+    $DB->delete("document_repository", array("record_id" => $rid));
+    $www = $config->getSetting('www');
+    $msg_data['deleteDocument'] = $www['url'] . "/main.php?test_name=document_repository";
+    $msg_data['document'] = $fileName;
+    $query_Doc_Repo_Notification_Emails = "SELECT Email from users where Active='Y' and Doc_Repo_Notifications='Y'";
+    $Doc_Repo_Notification_Emails = $DB->pselect($query_Doc_Repo_Notification_Emails, array());
+    foreach ($Doc_Repo_Notification_Emails as $email) {
+        Email::send($email['Email'], 'document_repository.tpl', $msg_data);
+    }
 }
 
 $path = "document_repository/" . $userName . "/" . $fileName;
